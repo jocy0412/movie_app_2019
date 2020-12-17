aee4fb56bd258accff9667cf20e85977ad88de4e
jocy0412 <jakob1@naver.com>
2020-12-17 18:07:13 +0900

    #6.3 sliceText, gh-pages, Router

---
+++ src/routes/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Detail = (props) => {
+    console.log(props);
+    return (
+        <div>
+            hello
+        </div>
+    );
+};
+
+export default Detail;
