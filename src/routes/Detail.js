2ad6327b81e7003a65e2220d0c4f8976eac7ff3f
jocy0412 <jakob1@naver.com>
2020-12-18 15:30:46 +0900

    #6.4 커밋

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const Detail = (props) => {
-    console.log(props);
-    return (
-        <div>
-            hello
-        </div>
-    );
-};
+class Detail extends React.Component{
+    componentDidMount(){
+        console.log(this.props);
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push("/");
+        }
+    }
+    render(){
+        const { location } = this.props;
+        if(location.state){
+            return <span>{location.state.title}</span>;
+        } else {
+            return null
+        }
+
+    }
+}
 
 export default Detail;
